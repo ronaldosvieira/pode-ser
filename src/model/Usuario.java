c8d4c8ad477a661661aa7ee19c075e814fa07542
Ronaldo Vieira <ronaldoesv@gmail.com>
2015-11-11 22:15:03 -0200

    Implementação classe Usuario

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,5 +1,24 @@
 package model;
 
 public class Usuario {
+	private String nome;
+	private int idade;
 	private Genero genero;
+	private String ocupacao;
+	private String cep;
+	
+	public Usuario(String nome, int idade, Genero genero, 
+			String ocupacao, String cep) {
+		this.nome = nome;
+		this.idade = idade;
+		this.genero = genero;
+		this.ocupacao = ocupacao;
+		this.cep = cep;
+	}
+	
+	public void alterarDados(String ocupacao, String cep, Integer idade) {
+		if (ocupacao != null) this.ocupacao = ocupacao;
+		if (cep != null) this.cep = cep;
+		if (idade != null) this.idade = idade;
+	}
 }
